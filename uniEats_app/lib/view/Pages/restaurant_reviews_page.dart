4867bb6b99045be69f44a3d7b3a47669ddc4191a
Rita Ramada <anaritaramada@gmail.com>
2022-05-24 20:39:48 +0100

    Review form

--- uniEats_app/lib/view/Pages/restaurant_reviews_page.dart
+++ uniEats_app/lib/view/Pages/restaurant_reviews_page.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:uni/model/entities/lecture.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_feedback/quick_feedback.dart';
 import 'package:uni/model/entities/meal.dart';
 import 'package:uni/model/entities/restaurant.dart';
 import 'package:uni/view/Widgets/page_title.dart';
@@ -35,25 +36,25 @@ class RestaurantReviewsPageView extends StatelessWidget {
     final Restaurant restaurant =
         ModalRoute.of(context).settings.arguments as Restaurant;
 
-    return Review();
+    return AddReview();
 
   }
 }
 
-class Review extends StatefulWidget{
-  const Review({key});
+class ReviewShower extends StatefulWidget{
+  const ReviewShower({key});
 
   @override
   State<StatefulWidget> createState() {
-   return ReviewState();
+   return ReviewShowerState();
   }
 
 }
 
-class ReviewState extends State<Review>{
+class ReviewShowerState extends State<ReviewShower>{
 
   final Stream<QuerySnapshot> reviews = FirebaseFirestore.instance.collection("reviews").snapshots();
-   final _reviewKey = GlobalKey<ReviewState>();
+   final _reviewKey = GlobalKey<ReviewShowerState>();
 
   @override
   Widget build(BuildContext context) {
@@ -86,6 +87,57 @@ class ReviewState extends State<Review>{
         ),
         
       );
+  }
+
+}
+
+class AddReview extends StatelessWidget{
+  void _showFeedback(context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return QuickFeedback(
+          title: 'Leave a Review', // Title of dialog
+          showTextBox: true, // default false
+          textBoxHint:
+              'Share your review', // Feedback text field hint text default: Tell us more
+          submitText: 'SUBMIT', // submit button text default: SUBMIT
+          onSubmitCallback: (feedback) {
+            print('$feedback'); // map { rating: 2, feedback: 'some feedback' }
+            Navigator.of(context).pop();
+          },
+          askLaterText: 'ASK LATER',
+          onAskLaterCallback: () {
+            print('Do something on ask later click');
+          },
+        );
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () => _showFeedback(context),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 0x75, 0x17, 0x1e),
+                  textStyle: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold)),
+                child: Text('Add Review',style: new TextStyle(
+                            fontSize: 15, color: Color.fromARGB(255, 0xfa, 0xfa, 0xfa)),),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
 }
