a6cd3470b6612740539f450963daa47c4fa0dda0
Rita Ramada <anaritaramada@gmail.com>
2022-05-24 20:13:57 +0100

    Getting info from firebase

--- uniEats_app/lib/view/Pages/restaurant_reviews_page.dart
+++ uniEats_app/lib/view/Pages/restaurant_reviews_page.dart
@@ -2,6 +2,8 @@ import 'package:intl/intl.dart';
 import 'package:logger/logger.dart';
 import 'package:tuple/tuple.dart';
 import 'package:uni/model/app_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:uni/model/entities/lecture.dart';
 import 'package:flutter/material.dart';
 import 'package:uni/model/entities/meal.dart';
@@ -33,7 +35,57 @@ class RestaurantReviewsPageView extends StatelessWidget {
     final Restaurant restaurant =
         ModalRoute.of(context).settings.arguments as Restaurant;
 
-    return Text("Reviews");
+    return Review();
 
   }
+}
+
+class Review extends StatefulWidget{
+  const Review({key});
+
+  @override
+  State<StatefulWidget> createState() {
+   return ReviewState();
+  }
+
+}
+
+class ReviewState extends State<Review>{
+
+  final Stream<QuerySnapshot> reviews = FirebaseFirestore.instance.collection("reviews").snapshots();
+   final _reviewKey = GlobalKey<ReviewState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Container(
+          key: _reviewKey,
+          padding: const EdgeInsets.symmetric(vertical:20, horizontal: 20),
+          child: StreamBuilder<QuerySnapshot>(
+              stream: reviews,
+              builder: (
+                  BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot,
+                  ){
+                if (snapshot.hasError){return Text("Something went wrong");}
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return Text("Loading");
+                }
+                final data = snapshot.requireData;
+
+                return ListView.builder(
+                    itemCount: data.size,
+                    itemBuilder: (context, index){
+                      return Text("The student ${data.docs[index]['studentID']} rated the restaurant ${data.docs[index]['restaurantID']} ${data.docs[index]['starRating']} stars.");
+                    }
+                );
+              }
+          ),
+        ),
+        
+      );
+  }
+
 }
