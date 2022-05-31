960fec47dc0ba8f5298ff7d36a75fc0de67ebca3
Ana Matilde Guedes Perez da Silva Barra <up201904795@fe.up.pt>
2022-05-31 21:05:14 +0100

    [Update] Restaurant Cards on Homepage now with distance to restaurant

--- uniEats_app/lib/controller/map_utils.dart
+++ uniEats_app/lib/controller/map_utils.dart
@@ -1,16 +1,98 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:logger/logger.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:vector_math/vector_math.dart';
 
 class MapUtils {
+  bool servicestatus = false;
+  bool haspermission = false;
+  LocationPermission permission;
+  Position position;
+  String lat = '', long = '';
+  StreamSubscription<Position> positionStream;
+
+  MapUtils() {}
 
   MapUtils._();
 
   static Future<void> openMap(double latitude, double longitude) async {
-    final String googleMapsUrl = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
-  
+    final String googleMapsUrl =
+        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+
     if (await canLaunch(googleMapsUrl)) {
       await launch(googleMapsUrl);
     } else {
       throw 'Could not open the map.';
     }
+  }
+
+  Future<Position> checkGps() async {
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if (servicestatus) {
+      permission = await Geolocator.checkPermission();
+
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('Location permissions are denied');
+        } else if (permission == LocationPermission.deniedForever) {
+          print("'Location permissions are permanently denied");
+        } else {
+          haspermission = true;
+        }
+      } else {
+        haspermission = true;
+      }
+
+      if (haspermission) {
+        return getLocation();
+      }
+    } else {
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    return Position(longitude: null, latitude: null);
+  }
+
+  Future<Position> getLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    this.long = position.longitude.toString();
+    this.lat = position.latitude.toString();
+
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 100, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    return position;
+  }
+
+  double getKmToCurLocation(double latitude, double longitude) {
+    double lat1 = radians(double.parse(this.lat));
+    double lon1 = radians(double.parse(this.long));
+
+    double lat2 = radians(latitude);
+    double lon2 = radians(longitude);
+
+    // Haversine formula
+    double dlon = lon2 - lon1;
+    double dlat = lat2 - lat1;
+    double a =
+        pow(sin(dlat / 2), 2) + cos(lat1) * cos(lat2) * pow(sin(dlon / 2), 2);
+
+    double c = 2 * asin(sqrt(a));
+
+    double r = 6371;
+
+    // calculate the result
+    double res = c * r;
+
+    return res;
   }
 }
--- uniEats_app/lib/view/Widgets/unieats_restaurant_card.dart
+++ uniEats_app/lib/view/Widgets/unieats_restaurant_card.dart
@@ -1,11 +1,15 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'dart:ffi';
+import 'dart:math';
 //import 'dart:html';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:logger/logger.dart';
+import 'package:path/path.dart';
 import 'package:tuple/tuple.dart';
+import 'package:uni/controller/map_utils.dart';
 import 'package:uni/controller/restaurant_fetcher/restaurant_fetcher_html.dart';
 import 'package:uni/model/app_state.dart';
 import 'package:flutter/material.dart';
@@ -21,14 +25,18 @@ import 'generic_card.dart';
 import 'package:intl/intl.dart';
 import 'package:uni/utils/constants.dart' as Constants;
 
-class UniEatsRestaurantCard extends GenericCard {
+class UniEatsRestaurantCard extends StatefulWidget {
   Restaurant restaurant;
   String day;
   bool isHomepage;
   String title;
 
-  final Stream<QuerySnapshot> restDB =
-      FirebaseFirestore.instance.collection('restaurants').snapshots();
+  Position pos;
+  var scheduleList;
+  var coords;
+  double distance;
+  bool isOpen = false;
+  final restDB = FirebaseFirestore.instance.collection('restaurants');
 
   UniEatsRestaurantCard(
     Restaurant restaurant,
@@ -42,32 +50,200 @@ class UniEatsRestaurantCard extends GenericCard {
         title = title,
         super(key: key);
 
-  UniEatsRestaurantCard.fromEditingInformation(
-      Key key, bool editingMode, Function onDelete)
-      : super.fromEditingInformation(key, editingMode, onDelete);
+  // UniEatsRestaurantCardState.fromEditingInformation(
+  //     Key key, bool editingMode, Function onDelete)
+  //     : widget.fromEditingInformation(key, editingMode, onDelete);
 
-  @override
-  String getTitle() => this.title;
+  String getTitle() => title;
 
-  @override
   onClick(BuildContext context) => Navigator.pushNamed(
         context,
         '/' + Constants.navRestaurant,
-        arguments: this.restaurant,
+        arguments: restaurant,
       );
 
   @override
-  Widget buildCardContent(BuildContext context) {
-    final Tuple2<Restaurant, String> tuple = Tuple2(restaurant, day);
-    return RequestDependentWidgetBuilder(
-        context: context,
-        contentGenerator: generateMeals,
-        content: tuple,
-        contentChecker: true,
-        onNullContent: Center(
-            child: Text('Não existem restaurantes para apresentar',
-                style: Theme.of(context).textTheme.headline4,
-                textAlign: TextAlign.center)));
+  UniEatsRestaurantCardState createState() => UniEatsRestaurantCardState();
+}
+
+class UniEatsRestaurantCardState extends State<UniEatsRestaurantCard> {
+  MapUtils mapUtils = MapUtils();
+  bool loading = false;
+  final double borderRadius = 10.0;
+  final double padding = 12.0;
+
+  @override
+  initState() {
+    super.initState();
+
+    fetchData();
+  }
+
+  fetchData() async {
+    setState(() {
+      loading = true;
+    });
+
+    if (widget.isHomepage) {
+      widget.pos = await mapUtils.checkGps();
+
+      var data = await widget.restDB.get();
+      int priceRange;
+
+      int index = -1;
+
+
+      for (int i = 0; i < data.size; i++) {
+
+        if (data.docs[i]['name'] == 'Cantina - Almoço' &&
+            widget.restaurant.name == 'Cantina - Jantar') {
+          priceRange = data.docs[i]['priceRange'];
+          widget.coords = data.docs[i]['coords'];
+          continue;
+        } else if (data.docs[i]['name'] == 'Cantina - Jantar' &&
+            priceRange != null) {
+          widget.scheduleList = data.docs[i]['schedule'];
+          index = i;
+          break;
+        } else if (data.docs[i]['name'] == widget.restaurant.name) {
+          priceRange = data.docs[i]['priceRange'];
+          widget.scheduleList = data.docs[i]['schedule'];
+          widget.coords = data.docs[i]['coords'];
+          index = i;
+          break;
+        }
+      }
+
+      if(widget.scheduleList != null){
+      //Compute isOpen
+      var now = DateTime.now();
+      var weekDay = now.weekday - 1;
+
+      var hours = now.hour;
+      var minutes = now.minute;
+
+
+      String scheduleToday = widget.scheduleList[weekDay];
+     
+      if (scheduleToday != 'Fechado' || scheduleToday == null) {
+        var dayScheSplit = scheduleToday.split(' ');
+        var opening = dayScheSplit[0].split(':');
+        var closing = dayScheSplit[2].split(':');
+
+        var openH = int.parse(opening[0]);
+        var openM = int.parse(opening[1]);
+
+        var closeH = int.parse(closing[0]);
+        var closeM = int.parse(closing[1]);
+
+        if (hours < openH || hours > closeH) {
+          widget.isOpen = false;
+        } else if (hours > openH && hours < closeH) {
+          widget.isOpen = true;
+        } else if (hours == openH && minutes >= openM) {
+          widget.isOpen = true;
+        } else if (hours == closeH && minutes < closeM) {
+          widget.isOpen = true;
+        }
+      } else {
+        widget.isOpen = false;
+      }
+
+      }
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Tuple2<Restaurant, String> tuple =
+        Tuple2(widget.restaurant, widget.day);
+
+    return GestureDetector(
+        onTap: () => widget.onClick(context),
+        child: Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            color: Color.fromARGB(0, 0, 0, 0),
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(this.borderRadius)),
+            child: Container(
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color.fromARGB(0x1c, 0, 0, 0),
+                        blurRadius: 7.0,
+                        offset: Offset(0.0, 1.0))
+                  ],
+                  color: Theme.of(context).dividerColor,
+                  borderRadius:
+                      BorderRadius.all(Radius.circular(this.borderRadius))),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: 60.0,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(this.borderRadius))),
+                  width: (double.infinity),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Flexible(
+                              child: Container(
+                            child: Text(widget.getTitle(),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline1
+                                    .apply(
+                                        fontSizeDelta: -53,
+                                        fontWeightDelta: -3)),
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            margin: EdgeInsets.only(top: 15, bottom: 10),
+                          )),
+                        ]
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Divider(
+                          height: 5,
+                          color: Color.fromARGB(255, 0x75, 0x17, 0x1e),
+                          ),
+                        ),
+                      Container(
+                        padding: EdgeInsets.only(
+                          left: this.padding,
+                          right: this.padding,
+                          bottom: this.padding,
+                        ),
+                        child: getCardContent(tuple, context),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )));
+  }
+
+  Widget getCardContent(tuple, context){
+    return loading
+        ? Center(child: Text('Loading...'))
+        : RequestDependentWidgetBuilder(
+            context: context,
+            contentGenerator: generateMeals,
+            content: tuple,
+            contentChecker: true,
+            onNullContent: Center(
+                child: Text('Não existem restaurantes para apresentar',
+                    style: Theme.of(context).textTheme.headline4,
+                    textAlign: TextAlign.center)));
   }
 
   Widget generateMeals(tuple, context) {
@@ -100,7 +276,7 @@ class UniEatsRestaurantCard extends GenericCard {
         break;
     }
     List<Meal> meals = restaurant.getMealsOfDay(dayOfWeek);
-    if(meals != null){
+    if (meals != null) {
       return Container(
           child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -108,71 +284,70 @@ class UniEatsRestaurantCard extends GenericCard {
       ));
     }
     return Container(
-      child: Text("Não existem refeições para apresentar neste dia.\n Pressione para ver refeiçóes de outros dias"));
-
+        child: Text(
+            "Não existem refeições para apresentar neste dia.\n Pressione para ver refeiçóes de outros dias"));
   }
 
   List<Widget> getMealRows(context, List<Meal> meals) {
     final List<Widget> rows = <Widget>[];
     bool click = false;
 
-    var scheduleList;
-    int priceRange;
-    var coords;
+    if (widget.isHomepage) {
+      //Compute Distance to current location && return
+      var distance = Geolocator.distanceBetween(widget.coords.latitude,
+          widget.coords.longitude, widget.pos.latitude, widget.pos.longitude);
+      String helpText;
 
-    if (isHomepage) {
-    rows.add(StreamBuilder(
-        stream: restDB,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Loading');
-          }
+      if (distance >= 1000.0) {
+        distance = distance * pow(10, -3);
+        helpText = ' km';
+      } else {
+        helpText = ' m';
+      }
 
-          final data = snapshot.requireData;
+      distance = double.parse((distance).toStringAsFixed(2));
 
-          for (int i = 0; i < data.size; i++) {
-            if (data.docs[i]['name'] == 'Cantina - Almoço' &&
-                restaurant.name == 'Cantina - Jantar') {
-              priceRange = data.docs[i]['priceRange'];
-              coords = data.docs[i]['coords'];
-              continue;
-            } else if (data.docs[i]['name'] == 'Cantina - Jantar' &&
-                priceRange != null) {
-
-              scheduleList = data.docs[i]['schedule'];
-              break;
-            } else if (data.docs[i]['name'] == restaurant.name) {
-
-              priceRange = data.docs[i]['priceRange'];
-              scheduleList = data.docs[i]['schedule'];
-              coords = data.docs[i]['coords'];
-              break;
-            }
-          }
-          return Row(
-        children: [
+      rows.add(
         Row(
           children: [
-            Icon(Icons.pin_drop_outlined),
-            Text((coords.latitude).toString() + ' - ' + (coords.longitude).toString(), style: TextStyle(fontSize: 9.0)), //for location to current place
+            Row(
+              children: [
+                Icon(Icons.pin_drop_outlined),
+                Text(distance.toString() + helpText,
+                    style: TextStyle(
+                        fontSize: 15.0, fontWeight: FontWeight.bold)), //for location to current place
+              ],
+            ),
+            Spacer(),
+            widget.scheduleList != null
+              ?  Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.timer_outlined),
+                Padding(padding: EdgeInsets.only(left: 2)),
+                widget.isOpen
+                    ? Text(
+                        'Aberto',
+                        style: TextStyle(
+                            color: Colors.green.shade900,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    : Text('Fechado',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 141, 15, 23),
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold))
+              ],
+            )
+            : Container(),
+            Spacer(),
+            FavoriteWidget(),
           ],
         ),
-        Spacer(),
-        Row(children: [
-          Icon(Icons.timer_outlined),
-          Text(scheduleList[DateTime.now().weekday - 1]) //for timeopen
-        ],),
-        Spacer(),
-        FavoriteWidget(),
-
-      ]);
-    }));
+      );
     }
 
-    final now = DateTime.now();
     if (meals == null) {
       return rows;
     }
@@ -193,9 +368,7 @@ class UniEatsRestaurantCard extends GenericCard {
   }
 }
 
-
 class FavoriteWidget extends StatefulWidget {
-
   @override
   _FavoriteWidgetState createState() => _FavoriteWidgetState();
 }
@@ -211,19 +384,18 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
         Container(
           padding: const EdgeInsets.all(0),
           child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isFavorited
-                ? const Icon(Icons.star)
-                : const Icon(Icons.star_border)),
-            color: Color.fromARGB(255, 0x75, 0x17, 0x1e),
-            onPressed: (){
-            setState(()
-            {
-              _isFavorited = !_isFavorited;
-            });
-          }),
-          ),
+              padding: const EdgeInsets.all(0),
+              alignment: Alignment.centerRight,
+              icon: (_isFavorited
+                  ? const Icon(Icons.star)
+                  : const Icon(Icons.star_border)),
+              color: Color.fromARGB(255, 0x75, 0x17, 0x1e),
+              onPressed: () {
+                setState(() {
+                  _isFavorited = !_isFavorited;
+                });
+              }),
+        ),
       ],
     );
   }
