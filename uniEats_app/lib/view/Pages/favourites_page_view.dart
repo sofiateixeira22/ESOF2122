635952780aefdb0d2aa8d76b62ceef68240b57f2
Rita Ramada <anaritaramada@gmail.com>
2022-06-04 22:29:11 +0100

    remove prints

--- uniEats_app/lib/view/Pages/favourites_page_view.dart
+++ uniEats_app/lib/view/Pages/favourites_page_view.dart
@@ -41,21 +41,6 @@ class FavouritesPageViewState extends UniEatsNoDrawerPageView {
   var userID;
 
 
-  Widget getContent(Context){
-    return loading
-        ? Center(child: Text('Loading...'))
-        : Container(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 5),
-              child: Text(
-                'Favoritos',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    .apply(fontSizeFactor: 1.3),
-              ),
-            );
-  }
-
   fetchFavs() async {
     setState(() {
       loading = true;
@@ -64,7 +49,6 @@ class FavouritesPageViewState extends UniEatsNoDrawerPageView {
     final favorites_all = querySnapshot.docs.map((doc) => doc.data()).toList();
     var userID = StoreProvider.of<AppState>(context).state.content['profile'].email.substring(0,11);
     var user_favorites = [];
-    print("[GET_FAV2]");
     for(var i = 0; i < favorites_all.length; i++){
       var studentID = (jsonDecode(jsonEncode(favorites_all[i]))['studentID']);
       if(studentID == userID){
@@ -75,7 +59,6 @@ class FavouritesPageViewState extends UniEatsNoDrawerPageView {
     setState(() {
       loading = false;
     });
-    print("END OF FETCH");
   }
 
   @override
@@ -117,8 +100,6 @@ class FavouritesPageViewState extends UniEatsNoDrawerPageView {
     //final Future<List> favsName = getFavorites(context);
 
     if (restNames != null) {
-      print("[MiniCard Object]");
-      print(all_rest.length);
       for (int i = 0; i < all_rest.length; i++) {
         for(int j = 0; j< restNames.length; j++){
           if(restNames[j]== all_rest[i].name){
